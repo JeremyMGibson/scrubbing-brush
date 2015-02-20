4a15c29185a260f46b0d92cd4b77971541d91dd5
Jeremy Gibson <jeremy.gibson@qut.edu.au>
2015-02-20 13:01:58 +1000

    Added entities and service placeholders

---
+++ grails-app/domain/scrubbing/brush/MeasureRun.groovy
@@ -0,0 +1,7 @@
+package scrubbing.brush
+
+class MeasureRun {
+
+    static constraints = {
+    }
+}
---
+++ grails-app/services/scrubbing/brush/MeasureService.groovy
@@ -0,0 +1,11 @@
+package scrubbing.brush
+
+import grails.transaction.Transactional
+
+@Transactional
+class MeasureService {
+
+    def serviceMethod() {
+
+    }
+}
