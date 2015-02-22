81ccd4f0baf8e06264f84ed9d2b9275c4d8eb34b
JeremyMGibson <jeremy.gibson@qut.edu.au>
2015-02-22 19:59:58 +1000

    Update MeasureResult.groovy Added fields

--- grails-app/domain/chia/MeasureResult.groovy
+++ grails-app/domain/chia/MeasureResult.groovy
@@ -1,7 +1,16 @@
 package chia
 
 class MeasureResult {
+	String reference
+	String errorData	
+	DateTime found
+	DateTime fixed
+	bool ignore
+		
+	static belongsTo = [measure:Measure]
 
-    static constraints = {
-    }
+	static constraints = {
+		reference blank:false
+		found defaultValue:"new DateTime()"
+	}
 }
