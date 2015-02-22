e7507f63ed2899365559ee6620eba586b6a88a66
JeremyMGibson <jeremy.gibson@qut.edu.au>
2015-02-22 20:33:15 +1000

    Update MeasureService.groovy Implemented psuedo code for run measure method

--- grails-app/services/chia/MeasureService.groovy
+++ grails-app/services/chia/MeasureService.groovy
@@ -5,7 +5,50 @@ import grails.transaction.Transactional
 @Transactional
 class MeasureService {
 
-    def runMeasure(Measure measure) {
-		measure.query
-    }
+	def runMeasure(Measure measure) {
+		def measureRun = new MeasureRun()
+		try{
+			//Load connection
+			measure.connection
+       		Connection conn = Class.forName("com.mysql.jdbc.Driver");
+        		conn = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "root");
+        		Statement stmt = conn.createStatement();
+			def results = stmt.executeQuery(measure.query);
+
+			def newErrorCount = 0
+			def oldErrorCount = 0
+			def fixedErrorCount = 0
+			def rebrokenErrorCount = 0
+			def errorRefs = []
+
+			results.each(result ->
+				def measureResult = MeasureResult.findByMeasureAndReference(measure, result[0])
+				def errorData = [:]
+				1..result.size().each(i ->
+					errorData[i-1] = result[i]
+				)
+				if (measureResult == null) {
+					measureResult = new MeasureResult(measure, result[0], errorData)
+					newErrorCount++
+				} else if (measureResult.ignore) {
+					//Skip this one
+				} else if (measureResult.fixed == null) {
+					oldErrorCount++ 
+				} else {
+					measureResult.fixed = null
+					measureResult.found = new DateTime()
+					rebrokenErrorCount++
+				}
+				measureResult.save()
+				errorRefs << measureResult.reference
+			)
+			def	fixedErrors = MeasureResult.findByMeasureAndRNoteferenceAndFixed(measure, errorRefs, null)
+			fixedErrors.each(error ->
+				error.fixed = new DateTime()
+				error.saved()
+
+	    	} catch(Exception e) {
+			
+		}
+	}
 }
