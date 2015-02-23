462c98ca7776255b1554b17b3dab75fb599a93b6
JeremyMGibson <jeremy.gibson@qut.edu.au>
2015-02-23 22:23:52 +1000

    Update MeasureService.groovy

--- grails-app/services/chia/MeasureService.groovy
+++ grails-app/services/chia/MeasureService.groovy
@@ -6,30 +6,41 @@ import java.sql.Statement
 
 import org.joda.time.DateTime
 
+@Transactional
 class MeasureService {
 
-	def runMeasure(Measure measure) {
+	def  runMeasure(Measure measure) {
+		def run = runQuery(measure)
+		if (measure.updateScript is not empty && run.success) {
+			runUpdate(measure)
+			def results = executeSql(measure.connection, measure.query)
+			run.fixedErrors = run.newErrors + run.oldErrors + run.rebrokenErrors - results.size()
+			run.save()
+		} 
+	}
+
+	def runUpdate(Measure measure) {
+		def errors = MeasureError.findByMeasureIdAndFixed(measure.id, null)
+		// Load the error references and their data into the correction Script and execute it
+	}
+
+	def runQuery(Measure measure) {
 		def measureRun = new MeasureRun()
 
 		measureRun.runtime = new DateTime()
 		measureRun.measure = measure
 		measureRun.runNumber = measure.runs.size()
 		try{
-			//Load connection
-			measure.connection
-       		Connection conn = Class.forName("com.mysql.jdbc.Driver")
-        	conn = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "root")
-        	Statement stmt = conn.createStatement()
-			def results = stmt.executeQuery(measure.query)
-
+			def results = executeSql(measure.connection, measure.query)
+	
 			def newErrorCount = 0
 			def oldErrorCount = 0
-			def fixedErrorCount = 0
 			def rebrokenErrorCount = 0
-			def errorRefs = []
+
+			def errors = MeasureError.findByMeasureIdAndFixed(measure.id, null)
 
 			results.each{result ->
-				def measureResult = MeasureResult.findByMeasureIdAndReference(measure.id, result[0])
+				def measureResult = errors[result[0]]
 				def errorData = [:]
 				(1..result.size()).each{i ->
 					errorData[i-1] = result[i]
@@ -37,27 +48,43 @@ class MeasureService {
 				if (measureResult == null) {
 					measureResult = new MeasureResult(measure, result[0], errorData)
 					newErrorCount++
-				} else if (measureResult.ignore) {
-					//Skip this one
-				} else if (measureResult.fixed == null) {
-					oldErrorCount++ 
 				} else {
-					measureResult.fixed = null
-					measureResult.found = new DateTime()
-					rebrokenErrorCount++
+					measureResult.errorData = errorData
+					errors.remove(result[0])
+
+				 	if (measureResult.ignore) {
+						//Skip this one
+					} else if (measureResult.fixed == null) {
+						oldErrorCount++ 
+					} else {												measureResult.fixed = null
+						measureResult.found = new DateTime()
+						rebrokenErrorCount++
+					}
 				}
 				measureResult.save()
 				errorRefs << measureResult.reference
 			}
-			def	fixedErrors = MeasureResult.findByMeasureIdAndReferenceAndFixed(measure.id, errorRefs, null)
-			fixedErrors.each{error ->
+			errors.each{error ->
 				error.fixed = new DateTime()
 				error.saved()
 			}
-	    	
+			measureRun.oldErrors = oldErrorCount
+			measureRun.newErrors = newErrorCount
+			measureRun.reappearingErrors = rebrokenErrorCount
+			measureRun.fixedErrors = errors.size()
 		} catch(Exception e) {
-			
-			
+			measureRun.success = false
+		} finally {
+			measureRun.save()
 		}
+
+		return measureRun
+	}
+
+	def executeSql(Connection connection, String query) {
+		//Load connection
+      		Connection conn = Class.forName("com.mysql.jdbc.Driver") 		conn = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "root")
+	 	Statement stmt = conn.createStatement()
+		return stmt.executeQuery(measure.query)
 	}
 }
